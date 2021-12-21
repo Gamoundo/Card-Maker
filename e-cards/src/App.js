d90b9fa7d37da17e7cfbb3098a471610693fd7e1
Akil Griffiths <akilgg@gmail.com>
2021-12-21 17:12:34 -0500

    added some basic comps
    made signin, and card comps

--- e-cards/src/App.js
+++ e-cards/src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import SignIn from './SignIn';
 
 function App() {
+
+  let user = localStorage.getItem("name")
   return (
     <div className="App">
       <h1>Card Maker</h1>
+      {user === null && <SignIn />}
     </div>
   );
 }
---
+++ e-cards/src/SignIn.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function SignIn() {
+    return(
+        <h2>Signup Form</h2>
+    )
+}
+
+export default SignIn;
