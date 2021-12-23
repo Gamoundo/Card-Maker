cede1813613c6cb557dbe8782250fbcb5f1e73bf
Akil Griffiths <akilgg@gmail.com>
2021-12-23 18:44:22 -0500

    updated sign in
    Signin can switch from login to signup

--- e-cards/src/SignIn.js
+++ e-cards/src/SignIn.js
@@ -1,9 +1,57 @@
-import React from "react";
+import React, { useState } from 'react'
 
 function SignIn() {
+    
+    const unregistered = () => {
+        setregister(
+            <form >
+                <div>
+                    <h1>Signup</h1>
+                    <input type="email" name="email" placeholder="Email" />
+                    
+                    </div>
+                    <div>
+                    <input type="password" name="password" placeholder="Password" />
+             
+                </div>
+                
+                
+                <input type="submit" value="Signup"  />
+
+                
+                
+                 
+            </form>
+        )
+    }
+
+
+    const [registered, setregister] =useState(
+        <form >
+                <div>
+                    <input type="email" name="email" placeholder="Email" />
+                    
+                    </div>
+                    <div>
+                    <input type="password" name="password" placeholder="Password" />
+             
+                </div>
+                
+                
+                <input type="submit" value="Sign In"  />
+
+                <button onClick={unregistered}> Create Profile</button>
+                
+                 
+            </form>
+    )
     return(
-        <h2>Signup Form</h2>
+        <div className="form">
+            {registered}
+        </div>
     )
 }
+
+
 
 export default SignIn;
