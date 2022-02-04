3b002701e2a5662b6ec15ffeb9df0cfe069dc2ff
Gamoundo <akilgg@gmail.com>
2022-02-04 15:28:37 -0500

    state updates as the file changes

--- e-cards/src/CardForm.js
+++ e-cards/src/CardForm.js
@@ -1,7 +1,7 @@
 import { React } from "react";
 import { useState } from "react/cjs/react.production.min";
 
-function CardForm(props) {
+function CardForm() {
     
     const [formData, setformData] = useState(
         {
@@ -11,25 +11,47 @@ function CardForm(props) {
             message: "",
             occas: ""
         }
+
     )
+    const handleChange = (e) => {
+        setformData((prevState) => {
+            return{
+                ...prevState,
+                [e.target.name]: e.target.value
+            }
+        }
+
+        )
+    }
+
+    const   handleFileChange = (e) => {
+        setformData((prevState) => {            
+             return{
+                ...prevState,            
+                [e.target.name]: e.target.files[0]            
+             }                      
+        }            
+    
+        )            
+    } 
     return(
         <div>
             <h1> Make Someone's Day</h1>
             <form>
                 <div>
-                    <input type='file' name='photo'/>
+                    <input onChange={handleFileChange} type='file' name='photo'/>
                 </div>
                 <div>
-                    <input value={formData.sender} type="text" name="sender"/>
+                    <input onChange={handleChange} value={formData.sender} type="text" name="sender"/>
                 </div>
                 <div>
-                    <input value={formData.occas} type="text" name="occas"/>
+                    <input onChange={handleChange} value={formData.occas} type="text" name="occas"/>
                 </div>
                 <div>
-                    <input value={formData.recipient} type="text" name="recipient"/>
+                    <input onChange={handleChange} value={formData.recipient} type="text" name="recipient"/>
                 </div>
                 <div>
-                    <input value={formData.message} type="text" name="message"/>
+                    <input onChange={handleChange} value={formData.message} type="text" name="message"/>
                 </div>
             </form>
         </div>
